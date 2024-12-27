d6d96e479663d84561f724553726055288e1323a
CantAdLH <luizhenriquecantad16@hotmail.com>
2024-12-27 20:53:11 -0300

    finalizada a parte de análises dos dados numéricos

--- scripts/trabalho2.R
+++ scripts/trabalho2.R
@@ -6,6 +6,8 @@ library(readr)
 library(tidymodels)
 library(ggplot2)
 library(RColorBrewer)
+library(ggiraph)
+library(ggtext)
 
 ## Carregar a base de dados:
 
@@ -61,7 +63,7 @@ trabalho2_dados_7 %>%
 
 # Histogramas
 # Seleção de colunas numéricas e cálculo de dimensões
-aux <- trabalho2_dados_7 |> select(where(is.numeric))
+aux <- trabalho2_dados_7 |> select(where(is.numeric))|> drop_na()
 num_vars <- ncol(aux)
 
 # Configuração da área de plotagem e margens
@@ -73,17 +75,17 @@ lapply(seq_along(aux), function(i) {
     aux[[i]],
     main = colnames(aux)[i],    # Nome da variável
     xlab = "Valores",           # Rótulo do eixo x
-    ylab = "Frequência",        # Rótulo do eixo y
+    ylab = "Densidade",         # Rótulo do eixo y
     col = "skyblue",            # Cor do histograma
-    border = "white"            # Cor das bordas
+    border = "white",           # Cor das bordas
+    freq = F
   )
   abline(v = mean(aux[[i]], na.rm = TRUE), col = "red", lwd = 2) # Linha da média
+  abline(v = median(aux[[i]], na.rm = TRUE),col = "#599861", lty = 2, lwd = 2) # Linha da mediana
 })
 
 # Reseta a área de plotagem ao padrão
 par(mfrow = c(1, 1))
-
-
 
 
 # Idade: Por conta da assimetria normalizar (observações de 50 e 60 anos altera a média)
@@ -108,12 +110,17 @@ trabalho2_dados_7|>
   select(where(is.numeric))|>
   reshape2::melt()|>
   ggplot(aes(x = variable,y = value, fill = variable))+
-  geom_boxplot()+
-  facet_wrap(~variable, scales = 'free_y', ncol = 2)+
+  geom_boxplot(width = 0.3)+
+  facet_wrap(~variable, scales = 'free', ncol = 2)+
   theme_bw()+
   guides(fill = 'none')+
   theme(axis.text.x = element_blank(),
-        axis.title = element_blank())
+        axis.title = element_blank(),
+        panel.grid = element_blank(),
+        strip.text = element_text(face = 'bold'),
+        axis.ticks.y = element_line(),
+        strip.background = element_rect(fill = 'white')
+  )
 
 
 ## Violin plot
@@ -122,26 +129,37 @@ trabalho2_dados_7|>
   select(where(is.numeric))|>
   reshape2::melt()|>
   ggplot(aes(x = variable,y = value, fill = variable))+
-  geom_violin()+
+  geom_violin(width = 0.3)+
   guides(fill = 'none')+
-  facet_wrap(~variable, scales = 'free_y', ncol = 2)+
+  facet_wrap(~variable, scales = 'free', ncol = 2)+
   theme_bw()+
   theme(axis.text.x = element_blank(),
         axis.title = element_blank(),
-        panel.border = element_blank() 
+        panel.grid = element_blank(),
+        strip.text = element_text(face = 'bold'),
+        axis.ticks.y = element_line(),
+        strip.background = element_rect(fill = 'white')
   )
 
-## Ver Corrleações entre atributos
+## Ver Corrleações entre variáveis numéricas:
 
 correlacoes<-trabalho2_dados_7|>
   drop_na()|>
   select(where(is.numeric))|>
-  cor()
+  cor(method = 'kendall')
 
-par(oma = c(0, 0, 0, 0))
+ggcorrplot::ggcorrplot(correlacoes,
+                       type = 'lower',
+                       hc.order = T,colors = c('red','white','blue'),
+                       lab = T,
+                       title = 'Matriz de correlação para variáveis numéricas')
 
-corrplot::corrplot.mixed(correlacoes, order = "hclust", tl.pos = "lt", 
-                         upper = "ellipse")
+## Mostrar o resumo de relações entre as variáveis numéricas:
+
+trabalho2_dados_7|>
+  select(where(is.numeric))|>
+  drop_na()|>
+  GGally::ggpairs(title = 'Resumo das relações entre variáveis numéricas')
 
 
 
@@ -161,25 +179,86 @@ cores<- c("#89C5DA", "#DA5724", "#74D944", "#CE50CA", "#3F4921", "#C0717C", "#CB
           "#D14285", "#6DDE88", "#652926", "#7FDCC0", "#C84248", "#8569D5", "#5E738F", "#D1A33D", 
           "#8A7C64", "#599861")
 
-trabalho2_dados_7|>
-  select(!where(is.numeric))|>
-  drop_na()|>
-  rownames_to_column()|>
-  reshape2::melt(id = 'rowname',value.name = 'value')|>
-  group_by(value)|>
-  mutate(n = n())|>
-    ggplot(aes(x = variable, fill = interaction(variable,value)))+
-  geom_bar(position = 'fill')+
-  guides(fill = 'none')+
-  theme_bw()+
-  theme(axis.text.x = element_text(angle = 35,
-                                   vjust = 1,
-                                   hjust = 1,
-                                   size = 14),
-        axis.title = element_blank())+
-  scale_fill_manual(values = cores)+
-  geom_text(aes(label = ..count..), stat = "count", position = position_fill(vjust = 0.5), size = 3)
+trabalho2_dados_7 |>
+  select(!where(is.numeric)) |>
+  drop_na() |>
+  rownames_to_column() |>
+  reshape2::melt(id = 'rowname', value.name = 'value') |>
+  group_by(variable, value) |>
+  summarise(n = n(), .groups = "drop") |>
+  group_by(variable) |>
+  mutate(prop = n / sum(n)) |>
+  ggplot(aes(x = variable, y = prop, fill = interaction(variable, value))) +
+  geom_bar(stat = "identity", position = "fill") +
+  geom_text_repel(aes(label = paste(scales::percent(prop, accuracy = 1),"\n",value)),
+                  position = position_fill(vjust = 0.5),
+                  size = 3,
+                  col = "black",
+                  direction = "y",fontface = 'bold') +
+  scale_fill_manual(values = cores) +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 35, vjust = 1, hjust = 1, size = 14),
+    axis.title = element_blank()
+  ) +
+  guides(
+    fill = "none")
 
+## Gráfico interativo de proporções:
+
+p<-trabalho2_dados_7 |>
+  select(!where(is.numeric)) |>
+  drop_na() |>
+  rownames_to_column() |>
+  reshape2::melt(id = 'rowname', value.name = 'value') |>
+  group_by(variable, value) |>
+  summarise(n = n(), .groups = "drop") |>
+  group_by(variable) |>
+  mutate(prop = n / sum(n)) |>
+  ggplot(aes(x = variable,
+             y = prop,
+             fill = interaction(variable, value)))+ 
+  geom_bar_interactive(aes(data_id = value,
+                           tooltip = value),
+                       stat = "identity",
+                       position = "fill"
+  ) +
+  geom_text_repel(aes(label = scales::percent(prop, accuracy = 1)),
+                  position = position_fill(vjust = 0.5),
+                  size = 8,
+                  col = "black",
+                  direction = "y",
+                  fontface = 'bold') +
+  scale_fill_manual(values = cores) +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 35, vjust = 1, hjust = 1, size = 14),
+    axis.title = element_blank()
+  ) +
+  guides(
+    fill = "none")
+
+
+girafe(ggobj = p,
+       width_svg = 22,
+       height_svg = 10,
+       options = list(
+         opts_hover(css = 'stroke'),
+         opts_hover_key(css = 'stroke;r:8pt;cursor:pointer;'),
+         opts_selection(css = 'fill;opacity:1',type = 'single', only_shiny = F),
+         opts_selection_key(css = 'r:5pt;opacity:1',type = 'single', only_shiny = F),
+         opts_selection_inv(css = 'opacity:0.1'),
+         opts_hover_theme(),
+         opts_tooltip(
+           offx = 0,
+           offy = 10,
+           css = "background-color:#555555;color:white;padding:10px;
+                  border-radius:10px;box-shadow:1px 1px 1px rgba(0.5,0.5,0.5,0.3);
+                  font-family:roboto;font-size:15px;", # características da caixa de texto
+           opacity = 0.7, # transparência da caixa
+           use_fill = F,use_cursor_pos = T)
+       )
+)
 
 ### Seleção de variáveis:
 
